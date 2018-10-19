ffabc7735747e31269d2870ee278706a6a5c7b05
Andrew Bjonnes <andrew@abjonnes.com>
2018-10-19 14:29:09 -0400

    Patch OAuth2Provider's server to remove OpenID handling

--- auth_proxy/extensions.py
+++ auth_proxy/extensions.py
@@ -5,8 +5,8 @@ To avoid circular imports with views and create_app(), extensions are
 instantiated here. They will be initialized (calling init_app()) in
 application.py.
 """
+from auth_proxy.oauth2 import PatchedOAuth2Provider
 from flask_login import LoginManager
-from flask_oauthlib.provider import OAuth2Provider
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CsrfProtect
 
@@ -14,4 +14,4 @@ from flask_wtf.csrf import CsrfProtect
 db = SQLAlchemy()
 csrf = CsrfProtect()
 login_manager = LoginManager()
-oauthlib = OAuth2Provider()
+oauthlib = PatchedOAuth2Provider()
